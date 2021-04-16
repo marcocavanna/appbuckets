25c007e50851e507a3547bba995b1ec723d11b42
Marco Cavanna <marco@appbuckets.io>
2021-04-16 17:51:30 +0200

    chore: storybook theme and configuration

--- storybook/.storybook/preview.tsx
+++ storybook/.storybook/preview.tsx
@@ -1,8 +1,20 @@
+import { addons } from '@storybook/addons';
+
 import '@appbuckets/react-ui/styles/index.scss';
 import './stories.scss';
 
+import theme from './theme';
+
+
+addons.setConfig({
+  theme
+});
 
 export const parameters = {
   actions : { argTypesRegex: '^on.*' },
-  controls: { expanded: true, sort: 'requiredFirst' }
+  controls: { expanded: true, sort: 'requiredFirst' },
+  options : {
+    storySort: (a: any, b: any) =>
+      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true })
+  }
 };
