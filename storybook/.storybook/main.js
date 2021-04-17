30381928d06222a1566b8b75d8e3ab00236d0a7f
Marco Cavanna <marco@appbuckets.io>
2021-04-17 12:38:32 +0200

    fix: set up webpack aliases due to refactored direct imports

--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
 
   stories: [ '../stories/*.stories.tsx' ],
@@ -7,6 +9,21 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/preset-scss',
     '@storybook/addon-postcss'
-  ]
+  ],
+
+  webpackFinal: ((config) => {
+
+    /** Update aliases */
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        ...config.resolve.alias,
+        '@appbuckets/react-ui/styles': path.resolve(__dirname, '..', '..', 'packages', 'react-ui', 'styles'),
+        '@appbuckets/react-ui'       : path.resolve(__dirname, '..', '..', 'packages', 'react-ui', 'src')
+      }
+    };
+
+    return config;
+  })
 
 };
