e52782f49351c0fa23db88994c532607080d6f2c
Marco Cavanna <marco@appbuckets.io>
2021-10-06 19:35:37 +0200

    chore: ported ButterToast props to NotificationContainer types

--- packages/react-ui-smart-components/src/NotificationContainer/NotificationContainer.types.ts
+++ packages/react-ui-smart-components/src/NotificationContainer/NotificationContainer.types.ts
@@ -1,7 +1,29 @@
-import { ButterToastProps } from 'butter-toast';
+import * as React from 'react';
 
 
-export interface NotificationContainerProps extends ButterToastProps {
+export type NotificationContainerHorizontalPosition = 'POS_RIGHT' | 'POS_CENTER' | 'POS_LEFT';
+export type NotificationContainerVerticalPosition = 'POS_TOP' | 'POS_CENTER' | 'POS_BOTTOM';
+
+
+export interface NotificationContainerProps {
+  /** Add className to tray container to add custom styling */
+  className?: string;
+
   /** Namespace is mandatory */
   namespace: string;
+
+  /** Set horizontal and vertical position of tray */
+  position?: {
+    horizontal: NotificationContainerHorizontalPosition,
+    vertical: NotificationContainerVerticalPosition
+  };
+
+  /** The distance in pixels between each toast in the tray */
+  spacing?: number;
+
+  /** Custom style object for the tray, eases custom positioning */
+  style?: React.CSSProperties;
+
+  /** The default time in ms for toasts in the tray. Can be overridden individually */
+  timeout?: number;
 }
