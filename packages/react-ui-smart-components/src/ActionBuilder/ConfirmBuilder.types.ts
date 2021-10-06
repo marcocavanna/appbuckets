e03c5dd66a6b948bf7025ccbbc9649810e1846aa
Marco Cavanna <marco@appbuckets.io>
2021-10-06 13:31:47 +0200

    feat: ConfirmBuilder

---
+++ packages/react-ui-smart-components/src/ActionBuilder/ConfirmBuilder.types.ts
@@ -0,0 +1,74 @@
+import * as React from 'react';
+
+import type {
+  BaseActionCancelHandler,
+  BaseActionCompletedHandler,
+  BaseActionHelpers,
+  BaseActionSubmitErrorHandler,
+  BaseActionSubmitHandler,
+  BaseActionBuilderActions,
+  BaseActionBuilderConfig,
+  BaseActionBuilderNotifications,
+  BaseActionBuilderProps
+} from './BaseActionBuilder.types';
+
+
+/* --------
+ * Utility Types
+ * -------- */
+type PlainOrBuilder<TOut, Props extends {}, Result> = TOut | ((props: ConfirmComponentProps<Props, Result>) => TOut);
+
+export interface ConfirmActionHelpers extends BaseActionHelpers {
+}
+
+
+/* --------
+ * Action Types
+ * -------- */
+export type ConfirmActionCancelHandler<Props extends {}> = BaseActionCancelHandler<ConfirmActionHelpers, Props>;
+
+export type ConfirmActionCompletedHandler<Props extends {}, Result> =
+  BaseActionCompletedHandler<Result, void, false, ConfirmActionHelpers, Props>;
+
+export type ConfirmActionSubmitHandler<Props extends {}, Result> =
+  BaseActionSubmitHandler<Result, void, false, ConfirmActionHelpers, Props>;
+
+export type ConfirmActionSubmitErrorHandler<Props extends {}> =
+  BaseActionSubmitErrorHandler<void, false, ConfirmActionHelpers, Props>;
+
+// eslint-disable-next-line max-len
+export type ConfirmActions<Props extends {}, Result> = BaseActionBuilderActions<ConfirmActionCancelHandler<Props>, ConfirmActionCompletedHandler<Props, Result>, ConfirmActionSubmitHandler<Props, Result>, ConfirmActionSubmitErrorHandler<Props>>;
+
+
+/* --------
+ * Notifications
+ * -------- */
+export interface ConfirmBuilderNotifications extends BaseActionBuilderNotifications {
+}
+
+
+/* --------
+ * Component Definition
+ * -------- */
+export type ConfirmComponentProps<Props extends {}, Result> =
+  BaseActionBuilderProps<Props, ConfirmActions<Props, Result>>;
+
+export type ConfirmComponent<Props extends {}, Result> = React.ComponentType<ConfirmComponentProps<Props, Result>>;
+
+export type ExtensibleConfirmComponent<Props extends {}, Result> =
+  & ConfirmComponent<Props, Result>
+  // eslint-disable-next-line max-len
+  & { extend: <NewProps extends {}, NewResult = any>(config: ConfirmBuilderConfig<NewProps, NewResult>) => ExtensibleConfirmComponent<NewProps, NewResult> };
+
+
+/* --------
+ * Configuration Types
+ * -------- */
+export interface ConfirmBuilderConfig<Props extends {}, Result>
+  extends BaseActionBuilderConfig<ConfirmComponent<Props, Result>, ConfirmActions<Props, Result>, ConfirmBuilderNotifications> {
+  /** The confirm modal default props to use */
+  defaultProps?: PlainOrBuilder<Partial<ConfirmComponentProps<Props, Result>>, Props, Result>;
+
+  /** The confirm modal override props object */
+  overrideProps?: PlainOrBuilder<Partial<ConfirmComponentProps<Props, Result>>, Props, Result>;
+}
---
+++ packages/react-ui-smart-components/src/ActionBuilder/index.ts
@@ -0,0 +1,14 @@
+export { default as buildConfirmAction } from './ConfirmBuilder';
+
+export type {
+  ConfirmActionHelpers,
+  ConfirmActionCancelHandler,
+  ConfirmActionCompletedHandler,
+  ConfirmActionSubmitHandler,
+  ConfirmActionSubmitErrorHandler,
+  ConfirmActions,
+  ConfirmBuilderNotifications,
+  ConfirmComponentProps,
+  ConfirmComponent,
+  ConfirmBuilderConfig
+} from './ConfirmBuilder.types';
